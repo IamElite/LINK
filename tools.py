d94e7bdff684d1c67cbb7c694b022fdf31319aed
IamElite <elitemindbusiness@gmail.com>
2025-06-27 09:52:20 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,7 +1,18 @@
 import time
+import asyncio
 from datetime import timedelta
-from pyrogram import enums
+from pyrogram import enums, filters
+from pyrogram.types import Message
+from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked
 from database import Database
+
+REPLY_ERROR = "<b>Use this command as a reply to any message</b>"
+
+async def full_userbase():
+    return []  # Placeholder - implement in database.py
+
+async def del_user(user_id):
+    pass  # Placeholder - implement in database.py
 
 async def handle_stats(client, message, db: Database, bot_start_time: float):
     """Provide bot statistics to the owner"""
@@ -23,3 +34,49 @@ async def handle_stats(client, message, db: Database, bot_start_time: float):
     )
     
     await message.reply(stats_message, parse_mode=enums.ParseMode.HTML)
+
+async def handle_broadcast(client: Client, message: Message, db: Database):
+    """Broadcast messages to all users"""
+    if message.reply_to_message:
+        query = await full_userbase()
+        broadcast_msg = message.reply_to_message
+        total = 0
+        successful = 0
+        blocked = 0
+        deleted = 0
+        unsuccessful = 0
+        
+        pls_wait = await message.reply("<i>Broadcasting Message.. This will Take Some Time</i>")
+        for chat_id in query:
+            try:
+                await broadcast_msg.copy(chat_id)
+                successful += 1
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+                await broadcast_msg.copy(chat_id)
+                successful += 1
+            except UserIsBlocked:
+                await del_user(chat_id)
+                blocked += 1
+            except InputUserDeactivated:
+                await del_user(chat_id)
+                deleted += 1
+            except:
+                unsuccessful += 1
+                pass
+            total += 1
+        
+        status = f"""<b><u>Broadcast Completed</u>
+
+Total Users: <code>{total}</code>
+Successful: <code>{successful}</code>
+Blocked Users: <code>{blocked}</code>
+Deleted Accounts: <code>{deleted}</code>
+Unsuccessful: <code>{unsuccessful}</code></b>"""
+        
+        return await pls_wait.edit(status)
+
+    else:
+        msg = await message.reply(REPLY_ERROR)
+        await asyncio.sleep(8)
+        await msg.delete()
